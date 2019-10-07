9983316936f78395e4a10c485438f7950fb2b4d0
jakebarnes <jakebarnes@noreply.githubusers.com>
2019-10-07 16:44:52 +0100

    [JB] finish http programming, start twirl templates

--- app/controllers/HttpProgrammingController.scala
+++ app/controllers/HttpProgrammingController.scala
@@ -48,4 +48,61 @@ class HttpProgrammingController @Inject()(cc: ControllerComponents) extends Abst
 
   //  val badRequest   = BadRequest(views.html.form(formWithErrors))
 
+
+  /* manipulating results */
+  val htmlResult2: Result = Ok(<h1>Hello World!</h1>).as(HTML)
+  def manipulateResult1: Action[AnyContent] = Action { implicit request =>
+    htmlResult2
+  }
+  def settingCookies: Action[AnyContent] = Action { implicit request =>
+    val cookieToUse = Cookie("cookieTest", "testValue")
+    Ok("Setting cookies :) \n" +
+      s"CMD+option+i > Application > Cookies > should contain: \nName: [${cookieToUse.name}], Value: [${cookieToUse.value}]")
+      .withCookies(cookieToUse)
+  }
+
+  /* storing data in session */
+  def setSessionData: Action[AnyContent] = Action { implicit request =>
+    Ok("Welcome!").withSession("connected" -> "user@gmail.com")
+  }
+
+  def readSession: Action[AnyContent] = Action { request =>
+    request.session
+      .get("connected")
+      .map { user =>
+        Ok(s"Hello $user")
+      }
+      .getOrElse {
+        Unauthorized("Oops you are not authorised or connected")
+      }
+  }
+
+  def clearSession: Action[AnyContent] = Action { request =>
+    Ok("Clearing session data").withNewSession
+  }
+
+  def flashTest = Action { implicit request =>
+    Ok {
+      request.flash.get("success").getOrElse("Welcome!")
+    }
+  }
+
+  def flashSave = Action {
+    Redirect("/home").flashing("success" -> "The item has been created")
+  }
+
+  def flashPartial = Action { implicit request =>
+    Ok(views.html.partials.flashPartial())
+  }
+
+  def jsonBodyParser1: Action[AnyContent] = Action { request: Request[AnyContent] =>
+    val body = request.body
+    val jsonBody = body.asJson
+    jsonBody.map {
+      json => Ok(s"Got: ${(json \ "name").as[String]}")
+    } .getOrElse {
+      BadRequest("Expected JSON in request body")
+    }
+  }
+
 }
